549745b44525e8ff702f0d17d086f469025dd7e0
BonneChaussure <lucasgaudin77pro@gmail.com>
2026-03-03 22:24:56 +0100

    first commit

---
+++ src/client/java/org/BonneChaussure/teststone/client/TeststoneClient.java
@@ -0,0 +1,10 @@
+package org.BonneChaussure.teststone.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TeststoneClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
