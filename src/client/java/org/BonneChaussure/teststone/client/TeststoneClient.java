6d6cb17c35f472bf8f764130463ddd08eb3692d4
BonneChaussure <lucasgaudin77pro@gmail.com>
2026-03-05 21:46:52 +0100

    fix: registered Network for client

--- src/client/java/org/BonneChaussure/teststone/client/TeststoneClient.java
+++ src/client/java/org/BonneChaussure/teststone/client/TeststoneClient.java
@@ -1,13 +1,26 @@
 package org.BonneChaussure.teststone.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
+import net.minecraft.util.math.BlockPos;
+import org.BonneChaussure.network.RemoveBoundaryBoxPacket;
+import org.BonneChaussure.network.SetBoundaryBoxPacket;
+import org.BonneChaussure.teststone.renderer.BoundaryBoxClientData;
 import org.BonneChaussure.teststone.renderer.BoundaryBoxRenderer;
 
 public class TeststoneClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        ClientPlayNetworking.registerGlobalReceiver(SetBoundaryBoxPacket.ID, (payload, context) -> {
+            BoundaryBoxClientData.boxes.put(payload.bench(), new BlockPos[]{payload.corner1(), payload.corner2()});
+        });
+
+        ClientPlayNetworking.registerGlobalReceiver(RemoveBoundaryBoxPacket.ID, (payload, context) -> {
+            BoundaryBoxClientData.boxes.remove(payload.bench());
+        });
+
         WorldRenderEvents.AFTER_TRANSLUCENT.register(BoundaryBoxRenderer::render);
     }
 }
